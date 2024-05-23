c0dfd6e605a0e3dd66524f9ef5cbe249d7d219df
Matheus Madin <nascimento.cruz@aluno.ifsp.edu.br>
2024-05-23 17:31:47 -0300

    edi3

--- src/controllers/musica/create.js
+++ src/controllers/musica/create.js
@@ -19,6 +19,7 @@ const create = async (req, res) => {
     } catch (error) {
         console.log(error)
         return res.status(500).json({
+
             error: 'Opsss erro no servidor, tente novamente!'
            
         })
